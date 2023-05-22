37fd58b50807ace4d812defa47010da9fef09fc7
Kelly Dance <kellybob3737@gmail.com>
2023-05-22 12:13:58 -0500

    rebrand + small tweaks

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define pb push_back
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
