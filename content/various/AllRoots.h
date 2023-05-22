f69dc85da83b7243b4c0800260b622265889f5e9
Kelly Dance <kellybob3737@gmail.com>
2023-05-22 18:51:51 -0500

    add all roots

---
+++ content/various/AllRoots.h
@@ -0,0 +1,65 @@
+/**
+ * Author: Shashank Bhatt & Kelly Dance
+ * License: CC0
+ * Source: Adapted from https://cses.fi/paste/17afc28084d1ac7755778e/
+ * Description: Applies a recurrence relation on a tree with every node as the root.
+ * Modify the merge function to combine children into a single representation.
+ * Modify the promote function to combine a child representation with its parent.
+ * Time: O(N) Assuming merge/promote are O(1).
+ * Status: TODO, hardly tested
+ */
+#pragma once
+
+typedef pair<ll,ll> T;
+struct AllRoots {
+	int n;
+	vector<vi> adj;
+	vector<vector<T>> pre, suf;
+	const T unit = {0, 0};
+	AllRoots(int n) : n(n), adj(n), pre(n), suf(n) {}
+	// !!!
+	T merge(const T& v1, const T& v2) {
+		return {v1.F + v2.F, v1.S + v2.S};
+	}
+	T promote(const T& v1) {
+		return {v1.F + v1.S, v1.S + 1};
+	}
+	void addEdge(int u, int v){
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+	T down(int x, int p = -1) {
+		int pi = -1;
+		if(p != -1){
+			pi = 0;
+			while(adj[x][pi] != p) ++pi;
+			swap(adj[x][0], adj[x][pi]);
+			pi = 0;
+		}
+		for(int i = adj[x].size()-1; i > pi; --i)
+			suf[x][i] = merge(down(adj[x][i], x), suf[x][i+1]);
+		return promote(suf[x][1]);
+	}
+	void up(int x, int p = -1) {
+		rep(i, p != -1, adj[x].size()) {
+			pre[x][i+1] = merge(pre[x][i],
+													promote(suf[adj[x][i]][1]));
+			pre[adj[x][i]][1] = promote(merge(pre[x][i],
+																				suf[x][i+1]));
+			up(adj[x][i], x);
+		}
+	}
+	vector<T> calc(int root = 0) {
+		rep(i,0,n) {
+			pre[i].assign(adj[i].size() + 1, unit);
+			suf[i].assign(adj[i].size() + 1, unit);
+		}
+		down(root);
+		if(sz(adj[root]))
+			pre[root][1] = promote(suf[adj[root][0]][1]);
+		up(root);
+		vector<T> res(n);
+		rep(i,0,n) res[i] = promote(pre[i].back());
+		return res;
+	}
+};
