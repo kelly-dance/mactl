929bbd1cc69b28d814184cc11e545619355456ad
Kelly Dance <kellybob3737@gmail.com>
2023-10-18 13:42:54 -0600

    Update ModularArithmetic.h

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -13,7 +13,7 @@
 const ll mod = 17; // change to something else
 struct Mod {
 	ll x;
-	Mod(ll xx) : x(xx) {}
+	Mod(ll xx) : x(xx % mod) {}
 	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
 	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
 	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
