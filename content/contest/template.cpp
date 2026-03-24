b8fb5b273470f84e7b0d40fdc24e9038394f634a
Kelly Dance <kellybob3737@gmail.com>
2026-03-23 22:42:59 -0400

    int is a 64 bit data type

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int ll
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
@@ -11,7 +12,7 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int main() {
+signed main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
 }
